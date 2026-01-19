02e157a0bd8cdb167cdc37931fc82f3171eb24d4
Muhammad Riza Rabbani <muhammad_riza_29rpl@student.smktelkom-mlg.sch.id>
2026-01-19 09:58:33 +0700

    init line chart

---
+++ app/src/main/java/org/riza0004/smartmeter20/ui/component/UsageReport.kt
@@ -0,0 +1,190 @@
+package org.riza0004.smartmeter20.ui.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.yml.charts.axis.AxisData
+import co.yml.charts.common.model.Point
+import co.yml.charts.ui.linechart.LineChart
+import co.yml.charts.ui.linechart.model.GridLines
+import co.yml.charts.ui.linechart.model.IntersectionPoint
+import co.yml.charts.ui.linechart.model.Line
+import co.yml.charts.ui.linechart.model.LineChartData
+import co.yml.charts.ui.linechart.model.LinePlotData
+import co.yml.charts.ui.linechart.model.LineStyle
+import co.yml.charts.ui.linechart.model.SelectionHighlightPoint
+import co.yml.charts.ui.linechart.model.SelectionHighlightPopUp
+import co.yml.charts.ui.linechart.model.ShadowUnderLine
+import org.riza0004.smartmeter20.R
+import org.riza0004.smartmeter20.ui.theme.SmartMeter20Theme
+
+@Composable
+fun UsageReport(
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(R.string.usage_report),
+                style = MaterialTheme.typography.titleMedium
+            )
+            Icon(
+                painter = painterResource(R.drawable.baseline_keyboard_arrow_right_24),
+                contentDescription = stringResource(R.string.show_more),
+                tint = colorResource(R.color.black),
+                modifier = Modifier.size(16.dp)
+            )
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth().weight(1f),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    modifier = Modifier.padding(end = 8.dp),
+                    text = stringResource(R.string.energy, 0.04),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = stringResource(R.string.power, 16),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            Card(
+                modifier = Modifier.wrapContentSize(),
+                colors = CardDefaults.cardColors(
+                    containerColor = colorResource(R.color.light_main),
+                    contentColor = colorResource(R.color.black)
+                ),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Row(
+                    modifier = Modifier.padding(vertical = 8.dp, horizontal = 4.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = stringResource(R.string.daily),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                    Icon(
+                        painter = painterResource(R.drawable.baseline_keyboard_arrow_down_24),
+                        contentDescription = stringResource(R.string.show_more),
+                        modifier = Modifier.size(16.dp)
+                    )
+                }
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Text(
+                modifier = Modifier.padding(end = 8.dp),
+                text = stringResource(R.string.outcome, 50000),
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Box(
+            modifier = Modifier.fillMaxWidth()
+        ){
+            LineChart()
+        }
+    }
+}
+
+@Composable
+fun LineChart(
+    modifier: Modifier = Modifier
+){
+    val pointsData: List<Point> = listOf(
+        Point(0f, 40f),
+        Point(1f, 90f),
+        Point(2f, 0f),
+        Point(3f, 60f),
+        Point(4f, 10f)
+    )
+    val xAxisData = AxisData.Builder()
+        .axisStepSize(100.dp)
+        .backgroundColor(colorResource(R.color.light_main))
+        .steps(pointsData.size-1)
+        .labelData { i -> i.toString() }
+        .labelAndAxisLinePadding(16.dp)
+        .build()
+
+    val steps = 4
+
+    val yAxisData = AxisData.Builder()
+        .steps(steps)
+        .backgroundColor(colorResource(R.color.light_main))
+        .labelAndAxisLinePadding(16.dp)
+        .labelData { i ->
+            val yScale = 100 / steps
+            ((i * yScale).toString())
+        }.build()
+
+    val lineChartData = LineChartData(
+        linePlotData = LinePlotData(
+            lines = listOf(
+                Line(
+                    dataPoints = pointsData,
+                    lineStyle = LineStyle(),
+                    intersectionPoint = IntersectionPoint(),
+                    selectionHighlightPoint = SelectionHighlightPoint(),
+                    shadowUnderLine = ShadowUnderLine(),
+                    selectionHighlightPopUp = SelectionHighlightPopUp()
+                )
+            )
+        ),
+        xAxisData = xAxisData,
+        yAxisData = yAxisData,
+        gridLines = GridLines(),
+        backgroundColor = colorResource(R.color.light_main)
+    )
+    LineChart(
+        modifier = modifier.fillMaxWidth().height(150.dp),
+        lineChartData = lineChartData
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UsageReportPreview() {
+    SmartMeter20Theme {
+        UsageReport()
+    }
+}
--- app/src/main/java/org/riza0004/smartmeter20/ui/theme/Type.kt
+++ app/src/main/java/org/riza0004/smartmeter20/ui/theme/Type.kt
@@ -9,36 +9,37 @@ import androidx.compose.ui.unit.sp
 import org.riza0004.smartmeter20.R
 
 // Set of Material typography styles to start with
-val EncodeSansSemiCondensed = FontFamily(
-    Font(R.font.encode_sans_semi_condensed_black),
-    Font(R.font.encode_sans_semi_condensed_extra_bold),
-    Font(R.font.encode_sans_semi_condensed_bold),
-    Font(R.font.encode_sans_semi_condensed_semi_bold),
-    Font(R.font.encode_sans_semi_condensed_regular),
-    Font(R.font.encode_sans_semi_condensed_medium),
-    Font(R.font.encode_sans_semi_condensed_light),
-    Font(R.font.encode_sans_semi_condensed_extra_light),
-    Font(R.font.encode_sans_semi_condensed_thin)
+val EncodeSansSemiCondensedBold = FontFamily(
+    Font(R.font.encode_sans_semi_condensed_bold)
 )
+
+val EncodeSansSemiCondensedSemiBold = FontFamily(
+    Font(R.font.encode_sans_semi_condensed_semi_bold)
+)
+
+val EncodeSansSemiCondensedRegular = FontFamily(
+    Font(R.font.encode_sans_semi_condensed_regular)
+)
+
 val Typography = Typography(
 
     // Display
     displayLarge = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
+        fontFamily = EncodeSansSemiCondensedBold,
         fontWeight = FontWeight.Bold,
         fontSize = 57.sp,
         lineHeight = 64.sp,
         letterSpacing = (-0.25).sp
     ),
     displayMedium = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
+        fontFamily = EncodeSansSemiCondensedBold,
         fontWeight = FontWeight.Bold,
         fontSize = 45.sp,
         lineHeight = 52.sp,
         letterSpacing = 0.sp
     ),
     displaySmall = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
+        fontFamily = EncodeSansSemiCondensedBold,
         fontWeight = FontWeight.SemiBold,
         fontSize = 36.sp,
         lineHeight = 44.sp,
@@ -47,21 +48,21 @@ val Typography = Typography(
 
     // Headline
     headlineLarge = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
+        fontFamily = EncodeSansSemiCondensedBold,
         fontWeight = FontWeight.SemiBold,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp
     ),
     headlineMedium = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
+        fontFamily = EncodeSansSemiCondensedBold,
         fontWeight = FontWeight.SemiBold,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp
     ),
     headlineSmall = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
+        fontFamily = EncodeSansSemiCondensedBold,
         fontWeight = FontWeight.Medium,
         fontSize = 24.sp,
         lineHeight = 32.sp,
@@ -70,21 +71,21 @@ val Typography = Typography(
 
     // Title
     titleLarge = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
+        fontFamily = EncodeSansSemiCondensedSemiBold,
         fontWeight = FontWeight.Medium,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     titleMedium = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
+        fontFamily = EncodeSansSemiCondensedSemiBold,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.15.sp
     ),
     titleSmall = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
+        fontFamily = EncodeSansSemiCondensedSemiBold,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
@@ -93,22 +94,22 @@ val Typography = Typography(
 
     // Body
     bodyLarge = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
-        fontWeight = FontWeight.Light,
+        fontFamily = EncodeSansSemiCondensedRegular,
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
-        fontWeight = FontWeight.Light,
+        fontFamily = EncodeSansSemiCondensedRegular,
+        fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
-        fontWeight = FontWeight.Light,
+        fontFamily = EncodeSansSemiCondensedRegular,
+        fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.4.sp
@@ -116,21 +117,21 @@ val Typography = Typography(
 
     // Label (buttons, chips, etc.)
     labelLarge = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
+        fontFamily = EncodeSansSemiCondensedRegular,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
+        fontFamily = EncodeSansSemiCondensedRegular,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = EncodeSansSemiCondensed,
+        fontFamily = EncodeSansSemiCondensedRegular,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
