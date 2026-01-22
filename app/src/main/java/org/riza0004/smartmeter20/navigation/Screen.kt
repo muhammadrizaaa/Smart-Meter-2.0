62b30a57872d0b38ecb308cf7cbce57edea2f7d8
Muhammad Riza Rabbani <muhammad_riza_29rpl@student.smktelkom-mlg.sch.id>
2026-01-22 16:24:12 +0700

    init ProfileScreen.kt

--- app/src/main/java/org/riza0004/smartmeter20/navigation/Screen.kt
+++ app/src/main/java/org/riza0004/smartmeter20/navigation/Screen.kt
@@ -2,5 +2,5 @@ package org.riza0004.smartmeter20.navigation
 
 sealed class Screen(val route: String) {
     data object HomeScreen: Screen("home_screen")
-    data object AuthScreen: Screen("auth_screen")
+    data object ProfileScreen: Screen("profile_screen")
 }
---
+++ app/src/main/java/org/riza0004/smartmeter20/ui/screen/profilescreen/ProfileScreen.kt
@@ -0,0 +1,11 @@
+package org.riza0004.smartmeter20.ui.screen.profilescreen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun ProfileScreen(
+    navHostController: NavHostController
+){
+
+}
